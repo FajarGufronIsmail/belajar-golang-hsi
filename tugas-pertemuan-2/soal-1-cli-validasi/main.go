3cd5e055f71391e6937843d3a416cbcaea9c3d30
Fajar Gufron Ismail <fajargufronism@gmail.com>
2025-07-21 16:05:41 +0700

    Memindahkan file ke folder tugas-pertemuan-2

---
+++ tugas-pertemuan-2/soal-1-cli-validasi/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	validErrName = errors.New("Isi nama Anda!")
+	validErrAge  = errors.New("Umur tidak valid (minimal 18 tahun)")
+)
+
+func getData(label string) string {
+	fmt.Print(label)
+	r := bufio.NewReader(os.Stdin)
+	s, _ := r.ReadString('\n')
+	return strings.TrimSpace(s)
+}
+
+func validateData(name string, age int) error {
+	if name == "" {
+		return validErrName
+	} else if age < 18 {
+		return validErrAge
+	} else {
+		return nil
+	}
+}
+
+func main() {
+	name := getData("Nama: ")
+	age, convErr := strconv.Atoi(getData("Umur: "))
+
+	err := validateData(name, age)
+	if err != nil {
+		if errors.Is(err, validErrName) {
+			fmt.Printf("Error: %s\n", validErrName)
+		} else if errors.Is(err, validErrAge) {
+			fmt.Printf("Error: %s\n", validErrAge)
+		}
+	}
+
+	if convErr != nil {
+		logrus.WithError(convErr).Error("Input umur tidak valid")
+	}
+
+	if err != nil {
+		logrus.WithError(err).Error("Data invalid")
+	} else {
+		fmt.Printf("Selamat datang, %s!", name)
+	}
+
+}
